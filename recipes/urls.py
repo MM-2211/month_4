510807a8962290c5c6fcb58ea57a6c79ba887f97
MM-2211 <pianolife2211@gmail.com>
2025-03-29 21:26:16 +0600

    test_edit

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('recipe_list/', views.RecipeListView.as_view(), name='recipe_list'),
-    path('recipe_list/<int:id>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe_list/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('create_recipe/', views.RecipeCreateView.as_view(), name='create_recipe'),
     path('delete_recipe/<int:id>/', views.RecipeDeleteView.as_view(), name='delete_recipe'),
     path('create_ingredient/<int:recipe_id>/', views.IngredientCreateView.as_view(), name='create_ingredient'),
